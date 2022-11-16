3a9a11b0e4a1b6bbe34b8a6118665f23baa3ad73
Justin Murrah <jdmurrah@gmail.com>
2022-11-15 23:56:56 -0600

    greetings, input box and p added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactDOM } from "react";
 import Greet from './components/Greeter'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 
 const App = () => {
-
+    const [username, setUsername] = useState(" ");
+    const handleTextChange = e => {
+        setUsername(e.target.value)
+    }
     return( 
     <div>
-        <Greet />
-        <input type="button" value="Greeting" />
+        <Greet greeting= "Hey Hey Hey, my name is" name= "Justin"/>
+        <Greet greeting= "Hey there bud! My name is" name= "Jen"/>
+        <Greet greeting= "I think my name is" name= "Jamie"/>
+        <input value={username} onChange={handleTextChange} className="card" />
+        <p>You are Logging in as: {username}</p>
     </div>
 )}
 
--- src/components/Greeter.jsx
+++ src/components/Greeter.jsx
@@ -5,9 +5,13 @@ import { ReactDOM } from 'react';
 
 const Greet = props => {
 
+
+
     return( 
     <div>
-        <h1>Hey</h1>
+        <h1 className= 'text-center'>{props.greeting} {props.name}</h1>
+        
+        <ul></ul>
     </div>
 )}
 
